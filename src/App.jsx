450a261099621b9201cdaaa298bac3566cf492de
MIYATAKR4 <catarina.cmd@gmail.com>
2023-11-13 19:52:02 -0300

    refactor: updated main archives

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import RoutesApp from "./routes";
+import './styles/Global.css';
+import { AuthProvider } from "./contexts/auth";
+
+const App = () => {
+    return (
+        <>
+        <AuthProvider>
+            <RoutesApp />
+        </AuthProvider>
+        </>
+    )
+}
+
+export default App;
